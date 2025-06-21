32d8da3fb91279c5f96caae5089c18f79ec97196
AthulRajagopal07 <87569106+AthulRajagopal07@users.noreply.github.com>
2025-06-21 06:52:30 +0100

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -20,18 +20,17 @@ class ZookeeperUser(User):
         try:
             self.zk.create(path, data)
             total_time = (time.time() - start) * 1000
-            self.environment.stats.log_request(
+            self.environment.events.request_success.fire(
                 request_type="znode",
                 name="create",
                 response_time=total_time,
-                response_length=len(data),
+                response_length=len(data)
             )
         except Exception as e:
             total_time = (time.time() - start) * 1000
-            self.environment.stats.log_request(
+            self.environment.events.request_failure.fire(
                 request_type="znode",
                 name="create",
                 response_time=total_time,
-                response_length=0,
                 exception=e
             )
